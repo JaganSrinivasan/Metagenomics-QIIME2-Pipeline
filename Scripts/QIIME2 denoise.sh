94b8fff73e4ebec54dd4ca52134872df5e6e8c36
Jagan Srinivasan <jagansrinivasanjk@gmail.com>
2026-06-13 12:05:38 +0530

    Create QIIME2 denoise.sh

---
+++ Scripts/QIIME2 denoise.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#Denoise with DADA2 (Adjust trunc-len parameters based on your quality plots)
+qiime dada2 denoise-paired \
+  --i-demultiplexed-seqs demux.qza \
+  --p-trim-left-f 0 \
+  --p-trim-left-r 0 \
+  --p-trunc-len-f 0 \
+  --p-trunc-len-r 0 \
+  --o-table table.qza \
+  --o-representative-sequences rep-seqs.qza \
+  --o-denoising-stats denoising-stats.qza && \
+  \
+  #for visualizing table.qza, rep-seq.qza and denoising-stats.qza
+  qiime demux summarize \
+  --i-data demux.qza\
+  --o-visualization demux-summary.qzv && \
+  \
+  qiime feature-table summarize \
+  --i-table table.qza\
+  --o- visualization table.qzv && \
+  \
+  qiime feature-table tabulate-seqs \
+  --i-data rep-seqs.qza \
+  --o-visualization rep-seqs.qzv && \
+  \
+  qiime metadata tabulate \
+  --m-input-file denoising-stats.qza \
+  --o-visualization denoising-stats.qzv
+  
