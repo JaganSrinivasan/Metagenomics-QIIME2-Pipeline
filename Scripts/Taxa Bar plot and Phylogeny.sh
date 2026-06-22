fc7e26bbde3fa7a6a0d5191908f33996ff1b1960
Jagan Srinivasan <jagansrinivasanjk@gmail.com>
2026-06-22 13:22:28 +0530

    Update Taxa Bar plot and Phylogeny.sh

--- Scripts/Taxa Bar plot and Phylogeny.sh
+++ Scripts/Taxa Bar plot and Phylogeny.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+#Tax-Bar-Plot
+
+qiime taxa barplot \
+  --i-table table.qza \
+  --i-taxonomy taxonomy.qza \
+  --m-metadata-file metadata.tsv \
+  --o-visualization taxa-bar-plots.qzv 
+  echo "Taxa Bar-plot generated" && \
+#Phylogeny analysis
+qiime phylogeny align-to-tree-mafft-fasttree \
+  --i-sequences rep-seqs.qza \
+  --o-alignment aligned-rep-seqs.qza \
+  --o-masked-alignment masked-aligned-rep-seqs.qza \
+  --o-tree unrooted-tree.qza \
+  --o-rooted-tree rooted-tree.qza 
+  echo "Tree constructed" 
