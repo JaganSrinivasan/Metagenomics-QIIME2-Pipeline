42ce9349547034a8fddae6a5756f75540fab1842
Jagan Srinivasan <jagansrinivasanjk@gmail.com>
2026-06-22 14:20:28 +0530

    Create 7. Alpha-rarefaction.sh

---
+++ Scripts/7. Alpha-rarefaction.sh
@@ -0,0 +1,8 @@
+!#bin/bash
+
+qiime diversity alpha-rarefaction \
+  --i-table table.qza \
+  --i-phylogeny rooted-tree.qza \
+  --p-max-depth 10000 \
+  --m-metadata-file metadata.tsv \
+  --o-visualization alpha-rarefaction.qzv
